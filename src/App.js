b8c26e966c7e11630ac0701e3b44245f706cb910
Pranav <pranavtrivedi02@gmail.com>
2023-01-19 19:32:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import * as THREE from "three"
+import { Suspense, Children, useLayoutEffect, useMemo, useRef } from "react"
+import { Canvas, useThree, useFrame } from "@react-three/fiber"
+import { ContactShadows, Loader, useTexture,useScroll,ScrollControls,Scroll} from "@react-three/drei"
+import { useTransition } from "@react-spring/core"
+import { a } from "@react-spring/three"
+
+import DistortionMaterial from "./DistortionMaterial"
+import { Container, Jumbo, Nav, Box, Line, Cover } from "./Styles"
+
+const square = new THREE.PlaneBufferGeometry(20,43,254,254)
+
+const material1 = new DistortionMaterial()
+
+const jumbo = {
+  "/": ["The sun", "is its father."],
+  "/knot": ["The moon", "its mother."],
+  "/bomb": ["The wind", "hath carried it", "in its belly."],
+}
+
+function Shape({ geometry, material, args, textures, opacity, color, shadowScale = [9, 1.5, 1], ...props }) {
+  const ref = useRef()
+  const { mouse, clock } = useThree()
+  const [ao, normal, height, roughness] = textures
+  const [rEuler, rQuaternion] = useMemo(() => [new THREE.Euler(), new THREE.Quaternion()], [])
+  useFrame(() => {
+    if (ref.current) {
+      rEuler.set((-mouse.y * Math.PI) / 50, (mouse.x * Math.PI) / 30, -Math.PI / 3)
+      ref.current.quaternion.slerp(rQuaternion.setFromEuler(rEuler), 0.1)
+      ref.current.material.time = clock.getElapsedTime() * 3
+    }
+  })
+  return (
+    <group {...props}>
+      <a.mesh
+        position={[-10,7,0]}
+        ref={ref}
+        args={args}
+        geometry={geometry}
+        material={material}
+        material-color="white"
+        material-aoMap={ao}
+        material-normalMap={normal}
+        material-displacementMap={height}
+        material-roughnessMap={roughness}
+        material-opacity={opacity}
+      />
+    </group>
+  )
+}
+
+function Shapes() {
+  
+  const textures = useTexture(["/ao.jpg", "/normal.jpg", "/height.png", "/roughness.jpg"])
+  useLayoutEffect(() => {
+    textures.forEach((texture) => ((texture.wrapT = texture.wrapS = THREE.RepeatWrapping), texture.repeat.set(4, 4)))
+  }, [textures])
+  return(
+    <group>
+      
+          <Shape geometry={square} material={material1} textures={textures} opacity={[1]} />
+        
+    </group>
+  )
+}
+
+function Text({ children, opacity }) {
+  return (
+    <Box style={{ opacity }}>
+      <h1>Hey</h1>
+      <h1>I am Pranav</h1>
+      {Children.toArray(children).map((text, index) => (
+        <Line key={index} style={{ transform: opacity.to((t) => `translate3d(0,${index * -50 + (1 - t) * ((1 + index) * 40)}px,0)`) }}>
+          <div></div>
+          <Cover style={{ transform: opacity.to((t) => `translate3d(0,${t * 100}%,0) rotateZ(-10deg)`) }} />
+        </Line>
+      ))}
+    </Box>
+  )
+}
+
+export default function App() {
+  // Current route
+  // Animated shape props
+  const [location] = "/"
+  // Animated shape props
+  const transition = useTransition(location, {
+    from: { position: [0, 0, -20], rotation: [0, Math.PI, 0], scale: [0, 0, 0], opacity: 0 },
+    enter: { position: [0, 0, 0], rotation: [0, 0, 0], scale: [1, 1, 1], opacity: 1 },
+    leave: { position: [0, 0, -10], rotation: [0, -Math.PI, 0], scale: [0, 0, 0], opacity: 0 },
+    config: () => (n) => n === "opacity" && { friction: 60 },
+  })
+  return (
+    <>
+      <Container>
+        <Jumbo>
+          {transition((style, location) => (
+            <Text open={true} t={style.t} opacity={style.opacity} children={jumbo[location]} />
+          ))}
+        </Jumbo>
+      </Container>
+      <Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
+        <spotLight position={[-10, 30, 40]} />
+        <spotLight position={[-60, 30, 40]} />
+        <Suspense fallback={null}>
+          <Shapes  />
+        </Suspense>
+      </Canvas>
+      
+      <Loader />
+    </>
+  )
+}
